28c38bb3ccde775eb2b6ecf45ab3130d7667f891
nizardhr <nizardhr5@gmail.com>
2025-07-31 20:19:12 +0100

    Fix Netlify build error for dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   optimizeFonts: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
